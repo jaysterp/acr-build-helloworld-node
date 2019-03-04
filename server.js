cba44230297611da942abdf37529ea7ca680eed0
Jeanine Burke <41650461+jaysterp@users.noreply.github.com>
2019-03-04 15:26:00 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
+#!/bin/sh
 
-console.log('Starting sleep')
-sleep(120*1000)
-console.log('Ending sleep')
+echo "sleeping"
+sleep 1000000
