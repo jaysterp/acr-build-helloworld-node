9a3f448c143f4aa36d6c49d9fe9b2526dd873cef
Jeanine Burke <41650461+jaysterp@users.noreply.github.com>
2019-03-04 12:46:28 -0800

    Add sleep to server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+console.log('Starting sleep')
+sleep(120*1000)
+console.log('Ending sleep')
+
 var http = require('http')
 
 var port = 80
