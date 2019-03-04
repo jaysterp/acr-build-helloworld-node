d8b98bb031a7376b6a9ac6910f5565fb05de0371
Jeanine Burke <41650461+jaysterp@users.noreply.github.com>
2019-03-04 15:01:49 -0800

    Update server.js

--- server.js
+++ server.js
@@ -10,17 +10,3 @@ function sleep(milliseconds) {
 console.log('Starting sleep')
 sleep(120*1000)
 console.log('Ending sleep')
-
-var http = require('http')
-
-var port = 80
-
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
-  response.end('Version: ' + process.env.NODE_VERSION + '\n')
-})
-
-server.listen(port)
-
-console.log('Server running at http://localhost:' + port)
